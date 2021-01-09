b87a2262fc37d53fee42ed1294d4f7134523875e
AidanJP <aidanpendlebury@gmail.com>
2021-01-09 22:56:39 +0000

    Adding back environment variable.

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL#, Email
+from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
 
@@ -13,7 +13,6 @@ class CreatePostForm(FlaskForm):
 
 
 class CreateUserForm(FlaskForm):
-    # email = StringField("Email", validators=[DataRequired(), Email()])
     email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     name = StringField("Name", validators=[DataRequired()])
@@ -21,7 +20,6 @@ class CreateUserForm(FlaskForm):
 
 
 class LogInForm(FlaskForm):
-    # email = StringField("Email", validators=[DataRequired(), Email()])
     email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Log in")
