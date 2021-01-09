a53e17799be9977e8dcad9d384192a78723e3b24
AidanJP <aidanpendlebury@gmail.com>
2021-01-09 22:52:58 +0000

    Attempt removal of email vaildator.

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL, Email
+from wtforms.validators import DataRequired, URL#, Email
 from flask_ckeditor import CKEditorField
 
 
@@ -13,14 +13,16 @@ class CreatePostForm(FlaskForm):
 
 
 class CreateUserForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    # email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Register")
 
 
 class LogInForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    # email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Log in")
 
